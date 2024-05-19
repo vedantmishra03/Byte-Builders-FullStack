4e20c5c633dd12e65c519daa7a7cad102b41c94a
Vedant Mishra <vm31may@gmail.com>
2024-05-19 11:20:01 +0530

    This is the final commit

---
+++ server/models/OTP.js
@@ -0,0 +1,59 @@
+const mongoose = require("mongoose");
+const mailSender = require("../utils/mailSender");
+const emailTemplate = require("../mail/templates/emailVerificationTemplate");
+
+const OTPSchema = new mongoose.Schema({
+	email: {
+		type: String,
+		required: true,
+	},
+	otp: {
+		type: String,
+		required: true,
+	},
+	createdAt: {
+		type: Date,
+		default: Date.now,
+		expires: 60 * 5, // The document will be automatically deleted after 5 minutes of its creation time
+	},
+});
+
+// Define a function to send emails
+// we have to create pre middleware for otp verification ... we have to create here,
+// we have to write nodemailer => in Utils folder ...
+
+// A function to send email=>>
+async function sendVerificationEmail(email, otp) {
+	// Create a transporter to send emails
+
+	// Define the email options
+
+	// Send the email
+	try {
+		const mailResponse = await mailSender(
+			email,
+			"Verification Email",
+			emailTemplate(otp)
+		);
+		console.log("Email sent successfully: ", mailResponse.response);
+	} catch (error) {
+		console.log("Error occurred while sending email: ", error);
+		throw error;
+	}
+}
+
+// write pre middleware => db me save hone se phle mail send krega 
+
+OTPSchema.pre("save", async function (next) {
+	console.log("New document saved to database");
+
+	// Only send an email when a new document is created
+	if (this.isNew) {
+		await sendVerificationEmail(this.email, this.otp);
+	}
+	next();
+});
+
+const OTP = mongoose.model("OTP", OTPSchema);
+
+module.exports = OTP;
